963e22c164494f34eed1f5341e73b8c4d2640530
konstantinovaw <kamikofuroraru@gmail.com>
2021-09-19 10:59:16 +0300

    lesson 6-7 comparing

--- src/main/java/com/sbrf/reboot/lesson7/utils/AccountUtils.java
+++ src/main/java/com/sbrf/reboot/lesson7/utils/AccountUtils.java
@@ -2,13 +2,20 @@ package com.sbrf.reboot.lesson7.utils;
 
 import com.sbrf.reboot.lesson7.classes.Account;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class AccountUtils {
-    public static void sortedById(List<Account> accounts) {
 
+    public static void sortedById(List<Account> accounts) {
+        accounts.sort(Comparator.comparing(Account::getId));
     }
 
     public static void sortedByIdDate(List<Account> accounts) {
+        accounts.sort(Comparator.comparing(Account::getId).thenComparing(Account::getCreateDate));
+    }
+
+    public static void sortedByIdDateBalance(List<Account> accounts) {
+        accounts.sort(Comparator.comparing(Account::getId).thenComparing(Account::getCreateDate).thenComparing(Account::getBalance));
     }
 }
